3174ae9cf6f387406f6cba4991a6d41fbdb86fb3
abaduna~ <artuelrey@gmail.com>
2024-05-31 15:08:45 -0300

    sadfoih2

---
+++ Documents/NetBeansProjects/SistemaDeGestionUsuario/src/main/java/com/mycompany/sistemadegestionusuario/SistemaDeGestionUsuario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadegestionusuario;
+
+/**
+ *
+ * @author artur
+ */
+public class SistemaDeGestionUsuario {
+
+      public static void main(String[] args){
+        Formulario ventana = new Formulario();
+        ventana.show();
+      }
+}
